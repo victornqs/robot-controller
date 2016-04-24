da99ccb1c5574235dbf28e6cefeaa01b5e2481e7
Victor Barreto <victornqs@gmail.com>
2016-04-24 12:21:06 -0300

    Teste Comunicação Rádio Turnigy 9X 1 Canal

---
+++ Comunicacao_Turnigy9x_1Canal.ino
@@ -0,0 +1,20 @@
+
+int Canal01 = 7;
+
+unsigned long RCanal01;
+
+void setup()
+{
+  Serial.begin(9600); // inicializa a comunicação serial
+  pinMode(Canal01, INPUT);
+}
+
+void loop()
+{
+RCanal01 = pulseIn(Canal01, HIGH);
+RCanal01 = map(RCanal01,1000,2000,0,10);
+Serial.print("Duracao: ");
+Serial.println(RCanal01);
+delay(1000);
+
+}
